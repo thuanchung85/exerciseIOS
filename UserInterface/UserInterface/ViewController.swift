917ae453e63b633aa80887b1956af2ef0fb9b218
Lương Thuận Chung <49476209+thuanchung85@users.noreply.github.com>
2019-04-26 20:39:35 +0700

    Add files via upload

--- UserInterface/UserInterface/ViewController.swift
+++ UserInterface/UserInterface/ViewController.swift
@@ -34,51 +34,92 @@ class ViewController: UIViewController {
         return nameseparatorline
     }()
     
+    
+   
+    
+    var runJust1 : Bool = true
+    ///////////////////
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        SetupUIPortrait()
-    }
-    
-    ///func setup for portrait
-    func SetupUIPortrait() {
-        let frame = self.view.safeAreaLayoutGuide.layoutFrame
-        let heightMax = frame.height
-        let widthMax = frame.width
         
-        //setup logoImage
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
         logoImage.image = UIImage(named: "fbLogo@1x")
         logoImage.contentMode = .scaleAspectFit
         logoImage.layer.cornerRadius = 10
         logoImage.layer.masksToBounds = true
         logoImage.translatesAutoresizingMaskIntoConstraints = false
-        logoImage.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        logoImage.widthAnchor.constraint(equalToConstant: 70).isActive = true
-        logoImage.heightAnchor.constraint(equalToConstant: 70).isActive = true
-        logoImage.topAnchor.constraint(equalTo: self.view.topAnchor, constant: (heightMax * 10)/100).isActive = true
         
-        //setup textfieldUserName
         textfieldUserName.translatesAutoresizingMaskIntoConstraints = false
         textfieldUserName.placeholder = NSAttributedString(string: "Email or phone number", attributes: [NSAttributedString.Key.foregroundColor : UIColor.gray]).string
         textfieldUserName.layer.cornerRadius = 10
         textfieldUserName.layer.masksToBounds = true
         textfieldUserName.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
-        
         textfieldUserName.layer.borderWidth = 0.5
-        textfieldUserName.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        textfieldUserName.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 50).isActive = true
-        textfieldUserName.widthAnchor.constraint(equalToConstant: (widthMax * 90)/100).isActive = true
-        textfieldUserName.heightAnchor.constraint(equalToConstant: 40).isActive = true
         
-        //setup textfieldPassword
         textfieldPassword.translatesAutoresizingMaskIntoConstraints = false
         textfieldPassword.placeholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor : UIColor.gray]).string
         textfieldPassword.isSecureTextEntry = true
         textfieldPassword.layer.cornerRadius = 10
         textfieldPassword.layer.masksToBounds = true
         textfieldPassword.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
-        
         textfieldPassword.layer.borderWidth = 0.5
+        
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.backgroundColor = UIColor(displayP3Red: 59/255, green: 89/255, blue: 152/255, alpha: 1)
+        loginButton.setTitle("Login", for: .normal)
+        loginButton.tintColor = .white
+        loginButton.layer.cornerRadius = 5
+        loginButton.layer.masksToBounds = true
+        
+        forgotPasswordButton.translatesAutoresizingMaskIntoConstraints = false
+        forgotPasswordButton.tintColor = .blue
+        forgotPasswordButton.setTitle("Forgot password?", for: .normal)
+        
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        backButton.tintColor = .blue
+        backButton.setTitle("Back", for: .normal)
+        
+        createNewAccountButton.translatesAutoresizingMaskIntoConstraints = false
+        createNewAccountButton.tintColor = .blue
+        createNewAccountButton.setTitle("Create new account", for: .normal)
+        createNewAccountButton.backgroundColor = .clear
+        createNewAccountButton.layer.cornerRadius = 10
+        createNewAccountButton.layer.masksToBounds = true
+        createNewAccountButton.layer.borderWidth = 1
+        createNewAccountButton.layer.borderColor = UIColor(red: 59/255, green: 89/255, blue: 152/255, alpha: 1).cgColor
+        
+        orLable.translatesAutoresizingMaskIntoConstraints = false
+        orLable.text = "OR"
+        orLable.textAlignment = .center
+        orLable.backgroundColor = .white
+        
+        self.view.addSubview(textFieldSeparatorLine)
+        
+        self.view.addSubview(textFieldSeparatorLine2)
+    }
+    
+    ///func setup for portrait
+    func SetupUIPortrait() {
+       
+        
+        //setup logoImage
+        
+        logoImage.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        logoImage.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        logoImage.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        logoImage.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50).isActive = true
+        
+        //setup textfieldUserName
+        
+        textfieldUserName.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        textfieldUserName.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 50).isActive = true
+        textfieldUserName.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        textfieldUserName.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        //setup textfieldPassword
+       
         
         textfieldPassword.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         textfieldPassword.topAnchor.constraint(equalTo: textfieldUserName.bottomAnchor, constant: 10).isActive = true
@@ -86,90 +127,202 @@ class ViewController: UIViewController {
         textfieldPassword.heightAnchor.constraint(equalTo: textfieldUserName.heightAnchor).isActive = true
         
         //setup loginButton
-        loginButton.translatesAutoresizingMaskIntoConstraints = false
-        loginButton.backgroundColor = UIColor(displayP3Red: 59/255, green: 89/255, blue: 152/255, alpha: 1)
-        loginButton.setTitle("Login", for: .normal)
-        loginButton.tintColor = .white
-        loginButton.layer.cornerRadius = 5
-        loginButton.layer.masksToBounds = true
+        
         loginButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         loginButton.topAnchor.constraint(equalTo: textfieldPassword.bottomAnchor, constant: 10).isActive = true
         loginButton.widthAnchor.constraint(equalTo: textfieldUserName.widthAnchor).isActive = true
         loginButton.heightAnchor.constraint(equalTo: textfieldUserName.heightAnchor).isActive = true
         
         //setup forgotPasswordButton
-        forgotPasswordButton.translatesAutoresizingMaskIntoConstraints = false
-        forgotPasswordButton.tintColor = .blue
-        forgotPasswordButton.setTitle("Forgot password?", for: .normal)
-        
+       
         forgotPasswordButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         forgotPasswordButton.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 10).isActive = true
         forgotPasswordButton.widthAnchor.constraint(equalTo: textfieldUserName.widthAnchor).isActive = true
         forgotPasswordButton.heightAnchor.constraint(equalTo: textfieldUserName.heightAnchor).isActive = true
         
         //setup backButton
-        backButton.translatesAutoresizingMaskIntoConstraints = false
-        backButton.tintColor = .blue
-        backButton.setTitle("Back", for: .normal)
-        
+       
         backButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         backButton.topAnchor.constraint(equalTo: forgotPasswordButton.bottomAnchor, constant: 5).isActive = true
         backButton.widthAnchor.constraint(equalTo: textfieldUserName.widthAnchor).isActive = true
         backButton.heightAnchor.constraint(equalTo: textfieldUserName.heightAnchor).isActive = true
         
         //setup createNewAccountButton
-        createNewAccountButton.translatesAutoresizingMaskIntoConstraints = false
-        createNewAccountButton.tintColor = .blue
-        createNewAccountButton.setTitle("Create new account", for: .normal)
         
         createNewAccountButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        createNewAccountButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -5).isActive = true
+        createNewAccountButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -10).isActive = true
         createNewAccountButton.widthAnchor.constraint(equalTo: textfieldUserName.widthAnchor).isActive = true
         createNewAccountButton.heightAnchor.constraint(equalTo: textfieldUserName.heightAnchor).isActive = true
         
-        createNewAccountButton.backgroundColor = .clear
-        createNewAccountButton.layer.cornerRadius = 10
-        createNewAccountButton.layer.masksToBounds = true
-        createNewAccountButton.layer.borderWidth = 1
-        createNewAccountButton.layer.borderColor = UIColor(red: 59/255, green: 89/255, blue: 152/255, alpha: 1).cgColor
-        
+       
         //setup orLable
-        orLable.translatesAutoresizingMaskIntoConstraints = false
-        orLable.text = "OR"
-        orLable.textAlignment = .center
-        orLable.backgroundColor = .white
+        
         orLable.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        orLable.bottomAnchor.constraint(equalTo: createNewAccountButton.topAnchor, constant: -10).isActive = true
+        orLable.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -80).isActive = true
         orLable.widthAnchor.constraint(equalToConstant: 50).isActive = true
+         orLable.heightAnchor.constraint(equalToConstant: 50).isActive = true
         
         ///make a line
-        self.view.addSubview(textFieldSeparatorLine)
-        self.view.sendSubviewToBack(textFieldSeparatorLine)
         textFieldSeparatorLine.heightAnchor.constraint(equalToConstant: 1).isActive = true
         textFieldSeparatorLine.widthAnchor.constraint(equalToConstant: 250).isActive = true
-        textFieldSeparatorLine.topAnchor.constraint(equalTo: orLable.bottomAnchor, constant: -10).isActive = true
+        textFieldSeparatorLine.topAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
         textFieldSeparatorLine.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0).isActive = true
-        
+        self.view.sendSubviewToBack(textFieldSeparatorLine)
         ///make a line2
-        self.view.addSubview(textFieldSeparatorLine2)
-       // self.view.sendSubviewToBack(textFieldSeparatorLine2)
+     
+       
         textFieldSeparatorLine2.heightAnchor.constraint(equalToConstant: 1).isActive = true
-        textFieldSeparatorLine2.widthAnchor.constraint(equalToConstant: widthMax*90/100).isActive = true
+        textFieldSeparatorLine2.widthAnchor.constraint(equalToConstant: 200).isActive = true
         textFieldSeparatorLine2.topAnchor.constraint(equalTo: textfieldUserName.bottomAnchor, constant: 4).isActive = true
         textFieldSeparatorLine2.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0).isActive = true
         
+        print("SetupUIPortrait")
+        
     }
-    
-    
+    ///func setup for portrait
+    func SetupUILanscape() {
+        //setup logoImage
+        
+        logoImage.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        logoImage.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        logoImage.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        logoImage.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 10).isActive = true
+        
+        //setup textfieldUserName
+        
+        textfieldUserName.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        textfieldUserName.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 20).isActive = true
+        textfieldUserName.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        textfieldUserName.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        //setup textfieldPassword
+        
+        
+        textfieldPassword.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        textfieldPassword.topAnchor.constraint(equalTo: textfieldUserName.bottomAnchor, constant: 10).isActive = true
+        textfieldPassword.widthAnchor.constraint(equalTo: textfieldUserName.widthAnchor).isActive = true
+        textfieldPassword.heightAnchor.constraint(equalTo: textfieldUserName.heightAnchor).isActive = true
+        
+        //setup loginButton
+        
+        loginButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        loginButton.topAnchor.constraint(equalTo: textfieldPassword.bottomAnchor, constant: 10).isActive = true
+        loginButton.widthAnchor.constraint(equalTo: textfieldUserName.widthAnchor).isActive = true
+        loginButton.heightAnchor.constraint(equalTo: textfieldUserName.heightAnchor).isActive = true
+        
+        //setup forgotPasswordButton
+        
+        forgotPasswordButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        forgotPasswordButton.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 7).isActive = true
+        forgotPasswordButton.widthAnchor.constraint(equalTo: textfieldUserName.widthAnchor).isActive = true
+        forgotPasswordButton.heightAnchor.constraint(equalTo: textfieldUserName.heightAnchor).isActive = true
+        
+        //setup backButton
+        
+        backButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        backButton.topAnchor.constraint(equalTo: forgotPasswordButton.bottomAnchor, constant: -8).isActive = true
+        backButton.widthAnchor.constraint(equalTo: textfieldUserName.widthAnchor).isActive = true
+        backButton.heightAnchor.constraint(equalTo: textfieldUserName.heightAnchor).isActive = true
+        
+        //setup createNewAccountButton
+        
+        createNewAccountButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        createNewAccountButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -10).isActive = true
+        createNewAccountButton.widthAnchor.constraint(equalTo: textfieldUserName.widthAnchor).isActive = true
+        createNewAccountButton.heightAnchor.constraint(equalTo: textfieldUserName.heightAnchor).isActive = true
+        
+        
+        //setup orLable
+        
+        orLable.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -180).isActive = true
+        orLable.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -5).isActive = true
+        orLable.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        orLable.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        
+    }
+   //func remove all contraints
+    func removeAllScontraints()
+    {
+        for constraint in self.view.constraints
+        {
+            self.view.removeConstraint(constraint)
+        }
+        
+        for constraint in logoImage.constraints
+        {
+            logoImage.removeConstraint(constraint)
+        }
+        
+        for constraint in textfieldUserName.constraints
+        {
+            textfieldUserName.removeConstraint(constraint)
+        }
+        
+        for constraint in textfieldPassword.constraints
+        {
+            textfieldPassword.removeConstraint(constraint)
+        }
+        
+        for constraint in loginButton.constraints
+        {
+            loginButton.removeConstraint(constraint)
+        }
+        
+        for constraint in forgotPasswordButton.constraints
+        {
+            forgotPasswordButton.removeConstraint(constraint)
+        }
+        
+        for constraint in backButton.constraints
+        {
+            backButton.removeConstraint(constraint)
+        }
+        
+        for constraint in createNewAccountButton.constraints
+        {
+            createNewAccountButton.removeConstraint(constraint)
+        }
+        
+        for constraint in orLable.constraints
+        {
+            orLable.removeConstraint(constraint)
+        }
+        
+        for constraint in textFieldSeparatorLine.constraints
+        {
+            textFieldSeparatorLine.removeConstraint(constraint)
+        }
+        
+        for constraint in textFieldSeparatorLine2.constraints
+        {
+            textFieldSeparatorLine2.removeConstraint(constraint)
+        }
+         runJust1 = true
+        print("remove all constraint")
+    }
   /////////////
     override func viewWillLayoutSubviews() {
         
         let oriantation = UIDevice.current.orientation
         if oriantation.isLandscape {
             print("Landscape mode")
-            
+            removeAllScontraints()
+            if runJust1 {
+                
+                SetupUILanscape()
+                self.view.needsUpdateConstraints()
+                runJust1 = false
+            }
         }else if oriantation.isPortrait {
               print("Portrait mode")
+          removeAllScontraints()
+            if runJust1 {
+                
+                SetupUIPortrait()
+               self.view.needsUpdateConstraints()
+                runJust1 = false
+            }
         }
         
     }
